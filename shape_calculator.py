36b21af2837a40ca50efee7b1ed5c8d247567bf9
jlajr36 <jlajr36@gmail.com>
2021-05-09 16:08:02 -0500

    First commit

---
+++ shape_calculator.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area():
+        return self.width * self.height
+
+    def set_width(width):
+        self.width = width
+
+    def get_perimeter():
+        return (self.width * 2) + (self.height * 2)
+
+class Square:
